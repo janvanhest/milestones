97641a6302de4bebb54c2e5f7295daab3b4621eb
jan van Hest <jan@travpromobile.com>
2023-06-12 19:54:27 +0000

    php artisan make:model MilestoneType --controller --resource --requests  --factory --api

---
+++ app/Http/Controllers/MilestoneTypeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreMilestoneTypeRequest;
+use App\Http\Requests\UpdateMilestoneTypeRequest;
+use App\Models\MilestoneType;
+
+class MilestoneTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMilestoneTypeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MilestoneType $milestoneType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMilestoneTypeRequest $request, MilestoneType $milestoneType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MilestoneType $milestoneType)
+    {
+        //
+    }
+}
