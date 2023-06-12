61fcf361706059c928b270994881ee22482b785a
jan van Hest <jan@travpromobile.com>
2023-06-12 20:30:22 +0200

    installed barryvdh/laravel-ide-helper

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,11 +9,14 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'visits' => Redis::incr('visits'),
+        "mysql_version" => DB::select("select version()")[0]->{'version()'}
+    ]);
 });
