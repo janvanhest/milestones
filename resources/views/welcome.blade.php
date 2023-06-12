58a0e457f83ce88dbd492a0c92cab9f404efd512
jan van Hest <jan@travpromobile.com>
2023-06-12 19:24:21 +0000

    created a controller with corresponding model for milestones, all routes respond with not implemented error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,8 @@
 <body class="antialiased">
     <h1>Milestones Api</h1>
+    @if(App::environment('local'))
+        <p> This is the local environment</p>
+    @endif
     <p>Visits {{$visits}}</p>
     <p>MySql version {{$mysql_version}}</p>
 </body>
