daaa253f9a9db49363fd04e3d90a4eb0629eec34
jan van Hest <jan@travpromobile.com>
2023-06-13 10:21:21 +0000

    Added migrations for milestone and milestone_type models

---
+++ database/migrations/2023_06_13_095701_create_milestones_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('milestones', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->date('date');
+            $table->enum('status', ['completed', 'in progress', 'upcoming']);
+            $table->foreignId('milestone_type_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('milestones');
+    }
+};
