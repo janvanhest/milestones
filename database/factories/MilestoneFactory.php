93cef07c324aeb179f1d95974cf5bdc7bb7a1a21
jan van Hest <jan@travpromobile.com>
2023-08-21 20:38:13 +0000

    created milestonefactory

--- database/factories/MilestoneFactory.php
+++ database/factories/MilestoneFactory.php
@@ -12,12 +12,18 @@ class MilestoneFactory extends Factory
     /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array<string, mixed> 
      */
-    public function definition(): array
+    public function definition()
     {
         return [
-            //
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'date' => $this->faker->date,
+            'status' => $this->faker->randomElement(['completed', 'in progress', 'upcoming']),
+            'milestone_type_id' => function () {
+                return \App\Models\MilestoneType::factory()->create()->id;
+            },
         ];
     }
 }
